8117f4b1bfc8fbf4630bb200936db464fe00ccdb
Chris Roderique <chrisrdrq@gmail.com>
2016-11-18 11:48:02 -0500

    second commit

--- battleship.js
+++ battleship.js
@@ -1,104 +1,81 @@
+var rows = 7;
+var cols = 7;
+var squareSize = 50;
 
 
-var ElementUtils = {
- computeElementWidth: function(selector) {
-   var styleText = this.getStyleText('.cell');
-   var ml = this.grabStyle(styleText, 'margin-left');
-   var mr = this.grabStyle(styleText, 'margin-right');
-   var pl = this.grabStyle(styleText, 'padding-left');
-   var pr = this.grabStyle(styleText, 'padding-right');
-   if (ml === 0 && mr === 0) {
-     var m = this.grabStyle(styleText, 'margin'); ml = m; mr = m;
-   }
-   if (pl === 0 && pr === 0) {
-     var p = this.grabStyle(styleText, 'padding'); pl = p; pr = p;
-   }
-   var w = this.grabStyle(styleText, 'width');
-   return ml + pl + w + pr + mr;
- },
- findCssRule: function(styleText, rule) {
-   var searchIndex = styleText.indexOf(rule);
-   var endIndex = styleText.indexOf(';', searchIndex);
-   var startIndex = searchIndex + rule.length + 1;
-   if (startIndex < endIndex && startIndex > -1) {
-     return styleText.substring(startIndex, endIndex).trim();
-   }
-   return undefined;
- },
- grabStyle: function(styleText, rule) {
-   var cssRule = this.findCssRule(styleText, rule);
-   if (rule) {
-     var values = cssRule.split(' ');
-     if (values.length > 1) {
-       var isMargin = rule === 'margin';
-       var isPadding = rule === 'padding';
-       if (isMargin || isPadding) {
-         if (values[0].indexOf('px') > -1) {
-           return this.parseNumber(values[1]);
-         }
-       }
-     } else {
-       if (values[0].indexOf('px') > -1) {
-         return this.parseNumber(values[0]);
-       }
-     }
-   }
-   if (rule.match('width|height|margin|padding|border')) {
-       return 0;
-   }
-   return undefined;
- },
- getStyleText: function(selector) {
-   var styleSheets = document.styleSheets[0];
-   var classes = styleSheets.rules || styleSheets.cssRules;
-   for (var x = 0; x < classes.length; x++) {
-     var curr = classes[x];
-     if (curr.selectorText === selector) {
-       return curr.cssText ? curr.cssText : curr.style.cssText;
-     }
-   }
-   return '';
- },
- parseNumber: function(value) {
-   var match = value.match(/\d+/);
-   return match.length > 0 ? parseInt(match[0], 10) : 0;
- }
-};
+var gameBoardContainer = document.getElementById("gameboard");
 
-var TableUtil = {
- createTable: function(rows, cols) {
-   var tableEl = this.createEl('DIV', 'table');
-   for (var row = 0; row < rows + 1; row++) {
-     this.createRow(tableEl, row, cols + 1);
-   }
-   tableEl.style.width = this.calculateTableWidth(cols + 1);
-   return tableEl;
- },
- createRow: function(tableEl, row, cols) {
-   var rowEl = this.createEl('DIV', 'row');
-   for (var col = 0; col < cols; col++) {
-     this.createCol(rowEl, row, col);
-   }
-   tableEl.appendChild(rowEl);
- },
- createCol: function(rowEl, row, col) {
-   var isX = col === 0;
-   var isY = row === 0;
-   var cls = isX || isY ? 'cell-outer' : 'cell-inner';
-   var txt = (isX && row > 0 ? String.fromCharCode(64 + row) : isY && col > 0 ? col : '').toString();
-   rowEl.appendChild(this.createEl('DIV', 'cell ' + cls, txt));
- },
- createEl: function(tagName, className, innerHTML) {
-   var el = document.createElement(tagName);
-   el.className = className;
-   el.innerHTML = innerHTML || '';
-   return el;
- },
- calculateTableWidth: function(cols) {
-   var elWidth = ElementUtils.computeElementWidth('.cell');
-   return ((elWidth + 1) * cols) + 'px';
- }
-};
+// make the grid columns and rows
+for (i = 0; i < cols; i++) {
+	for (j = 0; j < rows; j++) {
 
-var el = document.getElementById('TableBox');
-el.appendChild(TableUtil.createTable(10, 10));
+		// create a new div HTML element for each grid square and make it the right size
+		var square = document.createElement("div");
+		gameBoardContainer.appendChild(square);
+
+    // give each div element a unique id based on its row and column, like "s00"
+		square.id = 's' + j + i;
+
+		// set each grid square's coordinates: multiples of the current row or column number
+		var topPosition = j * squareSize;
+		var leftPosition = i * squareSize;
+
+		// use CSS absolute positioning to place each grid square on the page
+		square.style.top = topPosition + 'px';
+		square.style.left = leftPosition + 'px';
+	}
+}
+var hitCount = 0;
+
+/* create the 2d array that will contain the status of each square on the board
+   and place ships on the board (later, create function for random placement!)
+   0 = empty, 1 = part of a ship, 2 = a sunken part of a ship, 3 = a missed shot
+*/
+var gameBoard = [
+				[0,0,0,1,1,1,1],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,1],
+				[0,0,0,0,0,0,1],
+				[1,0,0,0,1,1,1],
+				[1,0,0,0,0,0,0],
+				]
+
+// set event listener for all elements in gameboard, run fireTorpedo function when square is clicked
+gameBoardContainer.addEventListener("click", fireTorpedo, false);
+
+// initial code via http://www.kirupa.com/html5/handling_events_for_many_elements.htm:
+function fireTorpedo(e) {
+    // if item clicked (e.target) is not the parent element on which the event listener was set (e.currentTarget)
+	if (e.target !== e.currentTarget) {
+        // extract row and column # from the HTML element's id
+		var row = e.target.id.substring(1,2);
+		var col = e.target.id.substring(2,3);
+        //alert("Clicked on row " + row + ", col " + col);
+
+		// if player clicks a square with no ship, change the color and change square's value
+		if (gameBoard[row][col] == 0) {
+			e.target.style.background = '#bbb';
+			// set this square's value to 3 to indicate that they fired and missed
+			gameBoard[row][col] = 3;
+
+		// if player clicks a square with a ship, change the color and change square's value
+		} else if (gameBoard[row][col] == 1) {
+			e.target.style.background = 'red';
+			// set this square's value to 2 to indicate the ship has been hit
+			gameBoard[row][col] = 2;
+
+			// increment hitCount each time a ship is hit
+			hitCount++;
+			// this definitely shouldn't be hard-coded, but here it is anyway. lazy, simple solution:
+			if (hitCount == 11) {
+				alert("You win!");
+			}
+
+		// if player clicks a square that's been previously hit, let them know
+		} else if (gameBoard[row][col] > 1) {
+			alert("You already fired at this location.");
+		}
+    }
+    e.stopPropagation();
+}
