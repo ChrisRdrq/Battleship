1e3e2ec1a3e73f0818af7de5cc486d6b40a7f03d
Chris Roderique <chrisrdrq@gmail.com>
2016-11-21 17:18:59 -0500

    Fifth commit

--- battleship.js
+++ battleship.js
@@ -77,7 +77,8 @@ var gameBoards = [ [
 
 var currentBoard = 0;
 var gameBoard = gameBoards[currentBoard];
-
+var splash= new Audio("betterMiss.wav");
+var hit= new Audio("hitSound.wav");
 gameBoardContainer.addEventListener("click", fireTorpedo, false);
 
 
@@ -89,16 +90,16 @@ function fireTorpedo(e) {
         var col = e.target.id.substring(2, 3);
 
 
-
+        //miss is blue
         if (gameBoard[row][col] == 0) {
             e.target.style.background = '#429bf4';
-
+            splash.play();
             gameBoard[row][col] = 3;
-
+            //hit is red
         } else if (gameBoard[row][col] == 1) {
             e.target.style.background = 'red';
             gameBoard[row][col] = 2;
-
+            hit.play();
             hitCount++;
 
             if (hitCount == 12) {
